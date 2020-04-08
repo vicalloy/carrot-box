c58048e910f8e833d7b1c6ae9cb301621f400f01
vicalloy <zbirder@gmail.com>
2020-04-08 20:18:50 +0800

    add test for CarrotModelBackend

--- carrot_box/hr/tests.py
+++ carrot_box/hr/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 
 from carrot_box.tests import BaseTests
+
+from .backends import CarrotModelBackend
 from .userparser import CarrotBoxUserParser
 
 User = get_user_model()
@@ -19,3 +21,10 @@ class UserSimpleParserTests(BaseTests):
     def test_dept_direct_leaders(self):
         users = CarrotBoxUserParser("dept_direct_leaders d[it]").parse()
         self.assertEqual(users[0], self.users['it_dept_leader'])
+
+
+class PermissionTests(BaseTests):
+    def test_get_all_permissions(self):
+        backend = CarrotModelBackend()
+        it = self.users['it']
+        backend.get_all_permissions(it)
